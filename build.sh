1b7e1421d3db6bd426aa041769e5141c0c60a756
hrchlhck <vpemfh7@gmail.com>
2020-12-21 10:26:35 -0800

    Uploading from remote

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+TAG=latest
+
+function build() {
+    local name=$1
+    local image=vpemfh7/hadoop-$name:$TAG
+    cd $([ -z "$2" ] && echo "./$name" || echo "$2") 
+    echo "-----------------------" building $image in $(pwd)
+    docker build -t $image . && docker push $image
+    cd -
+}
+
+# Build every directory passed as parameter to the script
+# E.g. ./build.sh base namenode
+# The command above will build images from ./base and ./namenode
+for dockerfile in $@; do
+    build $dockerfile
+done
