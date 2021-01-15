ee75d8d42ffd5ba6d764f25a8e075e5130186383
hrchlhck <vpemfh7@gmail.com>
2021-01-15 16:52:20 -0300

    Fixed build script

--- build.sh
+++ build.sh
@@ -4,19 +4,67 @@ set -e
 
 TAG=latest
 
-function build() {
+### FUNCTIONS ###
+# Check if line arguments has an argument. 
+# Refer to: https://stackoverflow.com/a/56431189/12238188
+function has_param() {
+	local flag=$1
+	shift
+	for arg; do
+		if [[ $arg == "$term" ]]; then
+			return 0	
+		fi
+	done
+	return 1
+}
+
+# Build a docker image
+function docker_build() {
     local name=$1
-    local image=vpemfh7/hadoop-$name:$TAG
-    cd $([ -z "$2" ] && echo "./$name" || echo "$2")
+    local prefix=$2
+    local image=vpemfh7/$prefix-$name:$TAG
+    cd $([ -z "$3" ] && echo "$prefix/$name" || echo "$3")
     echo "-------------------------" building image $image in $(pwd)
-    docker build -t $image . && docker push $image
+    docker build --rm -t $image . 
     cd -
 }
 
 
+function docker_push() {
+	local name=$1
+	local prefix=$2
+	docker push vpemfh7/$prefix-$name:$TAG
+}
+
+function build_all() {
+	local flag=$1
+	local base_dir=$2
+	local prefix=$base_dir
+
+	if [[ $flag == "-S" ]]; then
+		echo "Building subdirectories"
+		for dir in $(ls $base_dir); do
+			docker_build $dir $prefix
+		done
+	else
+		docker_build $1
+	fi
+}
+
+function push_all() {
+	local user=$1
+	local repo_prefix=$2
+	for image in $(ls $repo_prefix); do
+		docker push $user/$repo_prefix-$image
+	done
+}
+
 # Build every directory passed as parameter of the script
 # E.g. ./build.sh hibench namenode
 # The command above will build Dockerfiles inside ./hibench and ./namenode
-for dockerfile in $@; do
-    build $dockerfile
-done
+
+build_all "-S" "hadoop" 
+push_all "vpemfh7" "hadoop"
+#for dockerimage in $@; do
+#    docker_push $dockerimage
+#done
