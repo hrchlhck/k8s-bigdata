b22fdd419851454642a091e5b50f0450a747f4f9
hrchlhck <vpemfh7@protonmail.com>
2021-09-16 10:24:54 -0300

    Commented networking stuff related to issue #6

--- scripts/init-cluster.sh
+++ scripts/init-cluster.sh
@@ -36,10 +36,10 @@ sleep 10
 ################
 ## NETWORKING ##
 ################
-add_host namenode
-add_host historyserver
-add_host datanodes
-add_host resourcemanager
+#add_host namenode
+#add_host historyserver
+#add_host datanodes
+#add_host resourcemanager
 set_benchmark_input_size $INPUT_SIZE
 set_executor_memory "2g"
 set_executor_cores "4"
